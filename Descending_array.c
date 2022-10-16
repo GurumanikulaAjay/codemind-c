e99e59265e92dd3bf52c811dc5bca4af1a0c3312
GurumanikulaAjay <97425496+GurumanikulaAjay@users.noreply.github.com>
2022-10-16 14:22:42 +0530

    #include <stdio.h>     int main()     {         int i, j, a, n, number[100],c;         scanf("%d", &n);         for (i=0;i<n;++i)             scanf("%d", &number[i]);         for (i=0;i<n;++i)         {             for (j=i+1;j<n;++j)             {                 if (number[i]<number[j])                 {                    c++;                 }             }         }         if(c==0)         {             printf("yes");         }         else         {             printf("no");         }     }

--- Descending_array.c
+++ Descending_array.c
@@ -5,37 +5,22 @@
 
 
 
-
-
-
-
-
-
-
-
 #include <stdio.h>
     int main()
     {
- 
         int i, j, a, n, number[100],c;
         scanf("%d", &n);
-        for (i = 0; i < n; ++i)
+        for (i=0;i<n;++i)
             scanf("%d", &number[i]);
- 
-        for (i = 0; i < n; ++i) 
+        for (i=0;i<n;++i) 
         {
- 
-            for (j = i + 1; j < n; ++j)
+            for (j=i+1;j<n;++j)
             {
- 
-                if (number[i] < number[j]) 
+                if (number[i]<number[j]) 
                 {
- 
                    c++;
                 }
- 
             }
- 
         }
         if(c==0)
         {
